dc3804a70237cd7fc29f1211b9efff39aa9bcff9
Alvaro Mouriño <alvaro@mourino.net>
2011-09-16 17:22:03 -0300

    Few bugs fixed.

--- geolocation/js/lib/map.js
+++ geolocation/js/lib/map.js
@@ -5,44 +5,36 @@ dojo.provide("lib.map");
 dojo.declare("lib.map", null, {
     latitude: null,
     longitude: null,
-    googleKey: 'ABQIAAAAEvV7AQjZLv0itwLFSt0IzxRx4N6pI3W7lfSElFmUx1_Ykxmo6BR1eo8NoQmWcjf6hxgzVsq-sZYoTQ',
+    sensor: false,
 
     constructor: function(args) {
-        console.info(args);
         dojo.safeMixin(this, args);
-        console.info(this.latitude);
+        this.loadAPI();
     },
 
-    postMixInProperties: function() {
-        this.id=dijit.getUniqueId('map');
-        var script = dojo.byId('api');
+    loadAPI: function() {
+        this.id = dijit.getUniqueId('map');
+        var script = document.getElementById('gmapsapi');
         if(!script){
-            // create the callback handler script to redirect the callback to this widget
-            script = document.createElement("script");
-            script.type = "text/javascript";
-            script.text = "function googleCallback(data){ dijit.byId('"+this.id+"').googleCallback(data)}";
-            dojo.doc.getElementsByTagName("head")[0].appendChild(script);
-
             // create the script to load the google maps api
             script = document.createElement("script");
             script.type = "text/javascript";
-            script.id="api";
-            script.src = "http://maps.google.com/maps?file=api&v=2.x&sensor=false&key="+this.googleKey+"&async=2&callback=googleCallback";
+            script.id = "api";
+            script.src = "http://maps.googleapis.com/maps/api/js?sensor=" + this.sensor + "&callback=map.googleCallback";
             dojo.doc.getElementsByTagName("head")[0].appendChild(script);
         }
         else{
-            googleCallback();
+            this.googleCallback();
         }
     },
 
     googleCallback: function(data){
-        var mapNode = document.getElementByid('gmap');
-        mapNode.style.height = '460px';
-        mapNode.style.width = '400px';
-        // dojo.doc.body.appendChild(mapNode);
-
-        // FIXME: GMap2 is not defined.
-        var map = new GMap2(mapNode);
-        map.setCenter(new GLatLng(this.latitude, this.longitude), 13);
+        var latlng = new google.maps.LatLng(this.latitude, this.longitude);
+        var myOptions = {
+            zoom: 14,
+            center: latlng,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("gmap"), myOptions);
     }
 });
