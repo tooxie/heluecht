79f45cd5e04b4fd2710efdd283be0ed8417aa1d5
Alvaro Mouriño <alvaro@mourino.net>
2011-09-17 19:03:05 -0300

    Huge refactor.

--- geolocation/js/usr/base.js
+++ geolocation/js/usr/base.js
@@ -1,17 +1,60 @@
+"use strict";
+console.info(localStorage);
 requireModules();
 
 dojo.ready(function() {
     if(!checkAll()) {
-        alert('Upgrade browser.');
+        setTitle('Unsupported browser');
     } else {
+        loadPlatformTheme();
         if(navigator.onLine) {
-            isAndroid();
+            console.log('navigator online');
             online();
         } else {
+            console.log('navigator offline');
             offline();
         }
+        setupRefresh();
+        setupCache();
     }
 });
+
+
+// --- custom modules --- //
+
+function loadCustomModules() {
+    // Load custom views here.
+    dojo.require("sources.NewsView");
+    dojo.require("sources.WeatherView");
+}
+
+function renderCustomModules(update) {
+    // Put custom widgets here.
+    window.news = sources.NewsView({
+        update: update,
+        data: retrieve('news'),
+        container: document.getElementById('newsList')
+    });
+    window.weather = sources.WeatherView({
+        update: update,
+        data: retrieve('weather'),
+        currentContainer: document.getElementById('weatherCurrent'),
+        forecastContainer: document.getElementById('weatherForecast')
+    });
+}
+
+
+// --- functions --- //
+
+function requireModules() {
+    dojo.require("dojox.mobile.parser");
+    dojo.require("dojox.mobile");
+    dojo.requireIf(!dojo.isWebKit, "dojox.mobile.compat");
+
+    dojo.require('lib.map');
+
+    loadCustomModules();
+}
 
 function checkAll() {
     try {
@@ -29,89 +72,142 @@ function checkAll() {
     return true;
 }
 
+function loadPlatformTheme() {
+    var h = document.getElementsByTagName("head")[0],
+        isAndroid = navigator.userAgent.indexOf("Android") > -1,
+        l = document.createElement("link"),
+        themesURL;
+
+    l.setAttribute("rel", "stylesheet");
+    themesURL = "http://ajax.googleapis.com/ajax/libs/dojo/1.6.0/dojox/mobile/themes/";
+    l.setAttribute("href", themesURL + (isAndroid ? "android/android.css" : "iphone/iphone.css"));
+    h.insertBefore(l, h.firstChild);
+}
+
+function online(changedCity) {
+    if(changedCity === undefined) {
+        navigator.geolocation.getCurrentPosition(getAddress, geoError);
+    } else {
+        if(changedCity === true) {
+            console.log('city changed');
+            renderModules(true);
+        } else {
+            console.log('same city');
+            renderModules(false);
+        }
+    }
+}
+
+function offline() {
+    var address,
+        position = retrieve('position');
+
+    document.getElementById('refresh').style['display'] = 'none';
+    if(position == undefined) {
+        console.error('ERROR');
+        setTitle('ERROR', error=true);
+        return;
+    }
+
+    address = position['address'];
+    displayMenu(address.city + ', ' + address.country);
+    renderModules();
+}
+
+
+// --- events --- //
+
+function cacheUpdateReady() {
+    console.log('application cache updated');
+}
+
+function setupCache() {
+    window.applicationCache.addEventListener('updateready', cacheUpdateReady());
+    if(window.applicationCache.status === window.applicationCache.UPDATEREADY) {
+        cacheUpdateReady();
+    }
+}
+
+function setupRefresh() {
+    var refresh = document.getElementById('refresh');
+
+    window.addEventListener("offline", function(e) {
+        refresh.style['display'] = 'none';
+    })
+    window.addEventListener("online", function(e) {
+        refresh.style['display'] = 'block';
+    })
+    refresh.addEventListener("click", function(e) {
+        console.log('refreshing...');
+        setTitle('Loading...');
+        wipeStorage();
+        online();
+    })
+}
+
+
+// --- auxiliary functions --- //
+
 function getNamespace() {
     return 'catsei';
 }
 
-function online() {
-    navigator.geolocation.getCurrentPosition(success, error);
+function displayMenu(title) {
+    console.log('displaying title and menu');
+    setTitle(title);
+    document.getElementById('menuList').style.display = 'block';
 }
 
-function offline() {
-    // dojox.mobile.parser.parse('menu');
-    render();
-}
+function setTitle(msg, error) {
+    var title = document.getElementById('title');
 
-function render() {
-    // Put custom widgets here.
-    var coords = retrieve('position')['coords'];
-    window.map = lib.map({
-        latitude: coords.latitude,
-        longitude: coords.longitude
-    });
-    window.news = sources.NewsView({
-        online: navigator.onLine,
-        container: document.getElementById('newsList')
-    });
-    window.weather = sources.WeatherView({
-        online: navigator.onLine,
-        currentContainer: document.getElementById('weatherCurrent'),
-        forecastContainer: document.getElementById('weatherForecast')
-    });
-}
-
-function isAndroid() {
-    // Create a new LINK element, get reference to the HEAD tag which we'll inject it into
-    var l = document.createElement("link"), h = document.getElementsByTagName("head")[0];
-    // Is this Android?
-    var isAndroid = navigator.userAgent.indexOf("Android") > -1;
-    // isAndroid = true;
-    // Add the appropriate stylesheet designations.
-    l.setAttribute("rel", "stylesheet");
-    themesURL = "http://ajax.googleapis.com/ajax/libs/dojo/1.6.0/dojox/mobile/themes/";
-    l.setAttribute("href", themesURL + (isAndroid ? "android/android.css" : "iphone/iphone.css"));
-    // Inject into header.
-    h.insertBefore(l, h.firstChild);
-}
-
-function requireModules() {
-
-    dojo.require("dojox.mobile.parser");
-    dojo.require("dojox.mobile");
-    // dojo.require("dojox.mobile.ScrollableView");
-    dojo.requireIf(!dojo.isWebKit, "dojox.mobile.compat");
-
-    if(navigator.onLine) {
-        dojo.require('lib.map');
+    if(error === true) {
+        title.classList.add('error');
+    } else {
+        title.classList.remove('error');
     }
-
-    // Load custom views here.
-    dojo.require("sources.NewsView");
-    dojo.require("sources.WeatherView");
+    title.innerHTML = msg;
 }
 
-function error(GeoPositionError) {
-    var error_codes = {},
-        title = document.getElementById('title');
+function renderModules(update) {
+    var coords = retrieve('position')['coords'];
 
-    error_codes[0] = 'Unknown error';
-    error_codes[GeoPositionError.PERMISSION_DENIED] = 'Permission denied';
-    error_codes[GeoPositionError.POSITION_UNAVAILABLE] = 'Position unavailable';
-    error_codes[GeoPositionError.TIMEOUT] = 'Timeout';
+    console.log('rendering modules');
+    if(update === undefined) {
+        update = navigator.onLine;
+    }
+    if(update) {
+        console.log('modules update content');
+    } else {
+        console.log('modules don\'t update');
+    }
+    document.getElementById('menumap').addEventListener('click', function() {
+        if(!window.map && lib.map) {
+            window.map = lib.map({
+                latitude: coords.latitude,
+                longitude: coords.longitude
+            });
+        }
+    });
 
-    title.className = 'error';
-    title.innerHTML = error_codes[GeoPositionError.code];
+    renderCustomModules(update);
 }
 
-function success(position) {
+
+// --- geolocation --- //
+
+function getAddress(position) {
     var coords, query;
+
+    console.log('guessing city...');
     coords = position.coords.latitude + ', ' + position.coords.longitude;
     query = 'select admin1, country from geo.places where text="' + coords + '" limit 1';
     lib.yql(query, {
         load: function(geoData) {
             var city = geoData.results.place.admin1.content,
                 country = geoData.results.place.country.content;
-            store('position', {
+
+            updateCity({
                 address: {
                     city: city,
                     country: country
@@ -121,12 +217,47 @@ function success(position) {
                     longitude: position.coords.longitude
                 }
             });
-            document.getElementById('title').innerHTML = city + ', ' + country;
-            document.getElementById('menuList').style.display = 'block';
-            render();
         }
     });
 }
+
+function updateCity(position) {
+    var _position = retrieve('position'),
+        changed = false;
+
+    if(_position === undefined) {
+        changed = true;
+    } else {
+        if(_position.address.city !== position.address.city) {
+            changed = true;
+        }
+        if(_position.address.country !== position.address.country) {
+            changed = true;
+        }
+    }
+    if(changed) {
+        console.log('city changed, we are now in ' + position.address.city);
+    } else {
+        console.log('we are still in ' + position.address.city);
+    }
+    displayMenu(position.address.city + ', ' + position.address.country);
+    store('position', position);
+    online(changed);
+}
+
+function geoError(GeoPositionError) {
+    var error_codes = {};
+
+    error_codes[0] = 'Unknown error';
+    error_codes[GeoPositionError.PERMISSION_DENIED] = 'Permission denied';
+    error_codes[GeoPositionError.POSITION_UNAVAILABLE] = 'Position unavailable';
+    error_codes[GeoPositionError.TIMEOUT] = 'Timeout';
+
+    setTitle(error_codes[GeoPositionError.code], error=true);
+}
+
+
+// --- localStorage --- //
 
 function store(key, value) {
     var orig_val;
@@ -139,13 +270,27 @@ function store(key, value) {
         throw "StorageError: No value provided";
     }
 
-    orig_val = JSON.parse(localStorage.getItem(getNamespace()) || '{}');
+    orig_val = dojo.fromJson(localStorage.getItem(getNamespace()) || '{}');
     orig_val[key] = value;
-    localStorage.setItem(getNamespace(), JSON.stringify(orig_val));
+    localStorage.setItem(getNamespace(), dojo.toJson(orig_val));
+    console.log('stored ' + key);
 }
 
 function retrieve(key) {
     // TODO: This parses the JSON object everytime an item is requested. This
     // TODO: can/should be cached.
-    return JSON.parse(localStorage.getItem(getNamespace()))[key];
+    try {
+        if(key === undefined) {
+            return dojo.fromJson(localStorage.getItem(getNamespace()));
+        } else {
+            return dojo.fromJson(localStorage.getItem(getNamespace()))[key];
+        }
+    } catch(e) {
+        return undefined
+    }
+}
+
+function wipeStorage() {
+    console.log('destroying storage contents');
+    delete localStorage[getNamespace()];
 }
